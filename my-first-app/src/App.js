2446f41fa7279afdad9e6a398bfabff0e0e1f032
Karen1501 <karencsj0391@gmail.com>
2020-08-10 19:27:23 -0500

    clase 6 :octocat:

--- my-first-app/src/App.js
+++ my-first-app/src/App.js
@@ -1,19 +1,12 @@
 import React from "react";
 import "./App.css";
-
-// Components
-import SwitchExample from "./components/SwitchExample";
-import List from "./components/Lista";
-import Post from "./components/Post";
-
+// Screens
+import BlogNotes from "./screens/NotesBlog";
 function App() {
   return (
     <div className="App">
-      <SwitchExample />
-      <List />
-      <Post />
+      <BlogNotes />
     </div>
   );
 }
-
 export default App;
