1702a0129a71fe3ac1dc7d708b341deb9a2acc85
Karen1501 <karencsj0391@gmail.com>
2020-08-11 20:10:15 -0500

    React Router

--- my-first-app/src/App.js
+++ my-first-app/src/App.js
@@ -1,12 +1,63 @@
 import React from "react";
 import "./App.css";
+
+// RRD
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 // Screens
-import BlogNotes from "./screens/NotesBlog";
+import NotesBlog from "./screens/NotesBlog";
+import CurrencyConverter from "./screens/CurrencyConverter";
+
 function App() {
   return (
-    <div className="App">
-      <BlogNotes />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact={true} path="/">
+            <>
+              <h1>Home</h1>
+              <nav>
+                <ul>
+                  <li>
+                    <Link to="/notesBlog">Blog de notas</Link>
+                  </li>
+                  <li>
+                    <Link to="/currencyConverter">Conversor de divisas</Link>
+                  </li>
+                </ul>
+              </nav>
+            </>
+          </Route>
+          <Route path="/notesBlog">
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/currencyConverter">Conversor de divisas</Link>
+                </li>
+              </ul>
+            </nav>
+            <NotesBlog />
+          </Route>
+          <Route path="/currencyConverter">
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/notesBlog">Blog de notas</Link>
+                </li>
+              </ul>
+            </nav>
+            <CurrencyConverter />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
+
 export default App;
