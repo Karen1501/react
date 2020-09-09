2cdead0d34df46f8ac0dd6c3b5ae0761fc25c10e
Karen1501 <karencsj0391@gmail.com>
2020-09-09 00:25:00 -0500

    ":octocat:"

--- my-first-app/src/components/Post/index.js
+++ my-first-app/src/components/Post/index.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Reactstrap from "reactstrap";
-import "../Date/date.css";
+
+import "./post.css";
 import {
   Card,
   Button,
@@ -17,15 +17,17 @@ function Post(props) {
     <div>
       <CardColumns>
         <Card>
-          <CardBody class="card">
+          <CardBody class="card-body">
             <CardImg
               class="card-img-top"
               src="https://picsum.photos/seed/picsum/200/300"
               alt="Card image cap"
             />
-            <CardTitle>{props.CardTitle}</CardTitle>
-            <CardSubtitle>{props.CardSubtitle}</CardSubtitle>
-            <CardText>{props.CardText}</CardText>
+            <CardTitle class="card-title">{props.CardTitle}</CardTitle>
+            <CardSubtitle class="card-subtitle">
+              {props.CardSubtitle}
+            </CardSubtitle>
+            <CardText class="card-text">{props.CardText}</CardText>
             <Button>GO</Button>
           </CardBody>
         </Card>
